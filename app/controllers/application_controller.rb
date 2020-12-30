94b9b2794a48f88e221d347b4269798961bcc8ed
elijahsamuels <elijahsamuels@gmail.com>
2020-12-29 18:24:49 -0700

    commit tree 58adb85be3683081fa0a70fd176722a367206e81

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,27 +11,28 @@ class ApplicationController < Sinatra::Base
     erb :home
   end
 
+  # render the sign-up form view
   get '/registrations/signup' do
-
     erb :'/registrations/signup'
   end
-
+  
+  # responsible for handling the POST request that is sent when a user hits 'submit' on the sign-up form. It will contain code that gets the new user's info from the params hash, creates a new user, signs them in, and then redirects them/users/home
   post '/registrations' do
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
-    @user.save
-    session[:user_id] = @user.id
-
-    redirect '/users/home'
+    @user.save  # saves the new user data
+    session[:user_id] = @user.id # similar to the post/sessions, this logins in the user
+    redirect '/users/home' #after the new user has completed the signup, this will redirect them to their users/home page
   end
-
+  
+  # renders the login form
   get '/sessions/login' do
-
     # the line of code below render the view page in app/views/sessions/login.erb
     erb :'sessions/login'
   end
 
+  # responsible for receiving the POST request that gets sent when a user hits 'submit' on the login form. This route contains code that grabs the user's info from the params hash, looks to match that info against the existing entries in the user database, and, if a matching entry is found, signs the user in.
   post '/sessions' do
-    @user = User.find_by(email: params[:email], password: params[:password])
+    @user = User.find_by(email: params[:email], password: params[:password])  # find the correct user from the database and log them in
     if @user
       session[:user_id] = @user.id
       redirect '/users/home'
@@ -39,14 +40,16 @@ class ApplicationController < Sinatra::Base
     redirect '/sessions/login'
   end
 
+  # responsible for logging the user out by clearing the session hash.
   get '/sessions/logout' do
-    session.clear
+    session.clear #logs out the user by clearing the session
     redirect '/'
   end
 
+    # responsible for rendering the user's homepage view
   get '/users/home' do
-
     @user = User.find(session[:user_id])
+    puts params
     erb :'/users/home'
   end
 end
